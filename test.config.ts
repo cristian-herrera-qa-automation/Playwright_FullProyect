82573233f1109d924c360c661b5e7a7eda9be49d
crisherrera-pi <cristian.herrera@piconsulting.com.ar>
2025-12-08 14:18:19 -0300

    Primera Subida

---
+++ test.config.ts
@@ -0,0 +1,14 @@
+export class TestConfig {
+  appUrl = "http://localhost/opencart/upload/";
+  //appUrl="https://naveenautomationlabs.com/opencart"
+  //appUrl="https://tutorialsninja.com/demo/"
+
+  //valid login credentials
+  email = "holanada@gmail.com";
+  password = "test123!";
+
+  //product details
+  productName = "MacBook";
+  productQuantity = "2";
+  totalPrice = "$1,204.00";
+}
